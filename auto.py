c4cabc2c410272373153211d1c0d78837e536c8d
witling <laurin.kirbach@wuerth-it.com>
2020-02-02 23:05:58 +0100

    adapt example script to botlib

--- auto.py
+++ auto.py
@@ -1,51 +1,80 @@
-def auto_erkannt(abstand_min, abstand_max):
-    abstand = abstandssensor()
-    return abstand_min < abstand < abstand_max
+#!/usr/bin/python3
 
-def auto_rechts():
-    kamera.rechts()
-    return auto_erkannt(0, 1)
+from botlib.bot import Bot
+from botlib.sonar import Sonar
+from time import sleep
 
-def kreuzung_frei():
-    kamera.mitte()
-    return auto_erkannt(0, 1)
+class Vorfahrt:
+    APPROACH_POWER, DEFAULT_POWER = 30, 50
+    RIGHT_MIN, RIGHT_MAX = 50, 100
+    FRONT_MIN, FRONT_MAX = 50, 100
 
-def main():
-    # Solange wir keine Kreuzungslinie erkannt haben
-    # fahren wir weiter
-    while not kreuzungslinie.erkannt():
-        folge()
+    def __init__(self):
+        self._bot = Bot()
+
+    def sensor_in_range(self, sensor, vmin, vmax):
+        return vmin < self._bot.sonar().read(sensor) < vmax
     
-    # Kreuzungslinie erkannt
-    # Geschwindigkeit verringern
-    speed.decrease()
+    def is_right_free(self):
+        return self.sensor_in_range(Sonar.RIGHT45, self.RIGHT_MIN, self.RIGHT_MAX)
     
-    # Wurde ein Auto während des Annährens an 
-    # die Kreuzung erkannt
-    auto_an_kreuzung = False
+    def is_front_free(self):
+        return self.sensor_in_range(Sonar.FRONT, self.FRONT_MIN, self.FRONT_MAX)
 
-    # Solange wir keine Stoplinie erkannt haben
-    while not stoplinie.erkannt():
+    def wait_till_front_free(self):
+        while not self.is_front_free():
+            sleep(0.1)
+
+    def follow_line(self):
+        from threading import Thread
+
+        linetracker = self._bot.linetracker()
+
+        def follow():
+            for improve in linetracker:
+                if improve:
+                    self._bot.drive_steer(improve)
+                    # TODO: is this needed
+                    sleep(0.1)
+
+        thread = Thread(group=None, target=follow, daemon=True)
+        thread.start()
+
+    def run(self):
+        # Ein neuer Thread kümmert sich darum, dass das Auto
+        # der Linie folgt
+        self.follow_line()
+
+        # Solange wir keine Kreuzungslinie erkannt haben
+        # fahren wir weiter
+        self._bot.drive_power(self.DEFAULT_POWER)
+        
+        # Kreuzungslinie erkannt
+        # Geschwindigkeit verringern
+        self._bot.drive_power(self.APPROACH_POWER)
+        
+        # Wurde ein Auto während des Annährens an 
+        # die Kreuzung erkannt
+        car_detected = False
+
+        # Solange wir keine Stoplinie erkannt haben
+        # Stoplinie erkannt
+
+        # Haben wir ein Auto während des Annährens gesehen?
+        if car_detected:
+            # Wenn Ja, anhalten
+            self._bot.drive_power(0)
+
+            # Warten bis die Kreuzung frei ist
+            self.wait_till_front_free()
+
+            # Nochmal rechts gucken ob ein zweites Auto nachkommt
+            while not self.is_right_free():
+                # Wieder warten bis die Kreuzung frei ist
+                self.wait_till_front_free()
+        
         # Weiterfahren
-        folge()
-        # Merken, wenn rechts ein Auto erkannt wurde
-        if auto_rechts():
-            auto_an_kreuzung = True
-    
-    # Stoplinie erkannt
+        self._bot.drive_power(self.DEFAULT_POWER)
 
-    # Haben wir ein Auto während des Annährens gesehen?
-    if auto_an_kreuzung:
-        # Wenn Ja, anhalten
-        stop()
-        # Warten bis die Kreuzung frei ist
-        while not kreuzung_frei():
-            pass
-        # Nochmal rechts gucken ob ein zweites Auto nachkommt
-        while auto_rechts():
-            # Wieder warten bis die Kreuzung frei ist
-            kreuzung_frei()
-    
-    # Weiterfahren
-    speed.increase()
-    folge()
+if __name__ == '__main__':
+    Vorfahrt().run()
